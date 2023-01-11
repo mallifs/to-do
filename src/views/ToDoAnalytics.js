d49fd88e4c6f1f5ea8b6ce17f51474f3ea2d04cf
Leon simintei <mallfs@icloud.com>
2023-01-11 15:53:25 +0300

    Navbar and footer updated

---
+++ src/views/ToDoAnalytics.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+function ToDoAnalytics(){
+    return (<div>
+        <Navbar/>
+        <div>to do analytcs</div>
+        <Footer/>
+    </div>
+
+    )
+}
+export default ToDoAnalytics
