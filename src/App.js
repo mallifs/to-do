5b8d021a1c881cce8f80c8d23d310f23a05765cf
Leon simintei <mallfs@icloud.com>
2023-01-16 09:42:56 +0300

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
  import './App.css';
-//import Home from './views/Home';
+// import Home from './views/Home';
 import ToDoAnalytics from './views/ToDoAnalytics';
+import Home from './views/Home';
 function App() {
   return (
     <div className="App">
       <ToDoAnalytics/>
+      <Home/>
+      
     </div>
   );
 }
